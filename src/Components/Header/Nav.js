a16e9e11082463faab36f50e43f3cbb52e111bfe
ZionIt-Tech-Team <zionitedu@gmail.com>
2023-02-27 14:38:46 +0530

    first commit

---
+++ src/Components/Header/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './nav.css'
+function Nav() {
+  return (
+    <div className='Nav'>
+        <div className='logoDiv'>
+                <img src='https://i.pinimg.com/736x/b4/57/f0/b457f066fc90c56a0192a695a4e42c1b.jpg'className='navImg' />
+        </div>
+        <div className='title'>
+                <h3>E SERVENT</h3>
+        </div>  
+    </div>
+  )
+}
+
+export default Nav
